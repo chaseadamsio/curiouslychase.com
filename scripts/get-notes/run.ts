d8405b859ebcfb87dc310b39e1a7ce31f58e582b
Chase Adams <realchaseadams@gmail.com>
2023-06-13 19:20:30 -0600

    add script for notes copy

---
+++ scripts/get-notes/run.ts
@@ -0,0 +1,104 @@
+#!/usr/bin/env ts-node
+
+import fs from "fs";
+import os from "os";
+import matter from "gray-matter";
+import { format, parseISO } from "date-fns";
+import slugify from "slugify";
+
+const EMBED_LINK_REGEX = /!\[\[([a-zA-ZÀ-ÿ0-9-'?%.():&,+/€_! ]+)\]\]/g;
+
+const notesDir = `${os.homedir}/vault`;
+
+const imagesDir = `${notesDir}/00 - Meta/02 - Media`;
+
+const copied = {
+  notes: 0,
+  images: 0,
+};
+
+async function getDirContents(directory: string) {
+  const contents = fs.readdirSync(directory);
+
+  for (const filename of contents) {
+    const filepath = `${directory}/${filename}`;
+    const filestat = fs.statSync(filepath);
+
+    if (filestat.isDirectory()) {
+      getDirContents(filepath);
+    } else if (filepath.match(/\.mdx?/)) {
+      const content = fs.readFileSync(filepath, "utf-8");
+      const result = matter(content);
+
+      if (
+        result.data.status &&
+        result.data.status.toLowerCase() === "published"
+      ) {
+        result.data.pubDate =
+          result.data.date ?? format(filestat.birthtime, "yyyy-MM-dd");
+
+        result.data.updatedDate =
+          result.data.modified ?? format(filestat.mtime, "yyyy-MM-dd");
+
+        if (result.data.pubDate === result.data.updatedDate) {
+          delete result.data.updatedDate;
+        }
+
+        result.data.layout = "../../layouts/BlogPost.astro";
+
+        const foundEmbed = content.match(EMBED_LINK_REGEX);
+
+        if (foundEmbed?.length) {
+          for (const image of foundEmbed) {
+            const sanitizedImageName = image
+              .replace(/^\!\[\[/, "")
+              .replace(/\]\]$/, "");
+            const path = `${imagesDir}/${sanitizedImageName}`;
+            const file = fs.readFileSync(path);
+            fs.writeFileSync(
+              `${process.cwd()}/public/img/notes/${sanitizedImageName}`,
+              file
+            );
+            copied.images++;
+          }
+        }
+
+        const splitPath = filepath.split("/");
+        const filename = splitPath.pop();
+        if (!filename) {
+          throw new Error(`unable to get filename from ${filepath}`);
+        }
+
+        if (!result.data.title) {
+          result.data.title = filename.replace(/\.mdx?/, "");
+        }
+
+        result.data.slug = slugify(filename.replace(/\.mdx?/, ""), {
+          lower: true,
+          remove: /[\.\?"',\(\))]*/,
+        });
+
+        const folderPath = `${process.cwd()}/src/content/posts`;
+        fs.mkdirSync(folderPath, { recursive: true });
+        const newPath = slugify(filename, {
+          lower: true,
+          remove: /[\?"',\(\))]*/,
+        });
+
+        fs.writeFileSync(
+          `${folderPath}/${result.data.slug}.mdx`,
+          matter.stringify(result.content, result.data)
+        );
+        copied.notes++;
+      }
+    }
+  }
+}
+
+async function main() {
+  await getDirContents(notesDir);
+  console.log(`Notes copied: ${copied.notes}`);
+  console.log(`Images copied: ${copied.images}`);
+}
+
+main();
