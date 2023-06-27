d24976635b44b0e3c4637511af9be232eff78d3e
Chase Adams <sub+github@crca.fyi>
2023-06-26 20:06:51 -0600

    Update rss.xml.js
    Fix url 🫠

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -11,7 +11,7 @@ export async function get(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: `/posts/${post.slug}/`,
     })),
   });
 }
