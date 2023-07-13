1321c4b99ef81071f4d3d361a802dad0361207d5
Chase Adams <realchaseadams@gmail.com>
2023-07-13 16:21:41 -0600

    add get post count function

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -20,3 +20,9 @@ export const getPosts = async (props?: { limit?: number }) => {
 
   return posts;
 };
+
+export const getPostsCount = async () => {
+  let allPosts = await getCollection("posts");
+
+  return allPosts.length;
+};
