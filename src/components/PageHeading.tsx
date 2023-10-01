c234936ed47d516bef468ed62599f3cb933db56e
Chase Adams <realchaseadams@gmail.com>
2023-10-01 16:47:41 -0600

    fix responsive page title size

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -2,7 +2,10 @@ import { FC, PropsWithChildren } from "react";
 
 export const PageHeading: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <h1 className="scroll-m-20 text-4xl font-bold tracking-tight" id="overview">
+    <h1
+      className="scroll-m-20 text-3xl md:text-4xl font-bold tracking-tight"
+      id="overview"
+    >
       {children}
     </h1>
   );
