2d42308a9e6f483db500d68a1ff3516364c51da7
Chase Adams <realchaseadams@gmail.com>
2023-10-01 17:04:01 -0600

    use cards

--- src/components/PostSummaryList.tsx
+++ src/components/PostSummaryList.tsx
@@ -1,16 +1,33 @@
 import { FormattedDate } from "@/components/FormattedDate";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { Article } from "@/utils/content/getArticles";
 import Link from "next/link";
 
 export const PostSummaryList = ({ posts }: { posts: Array<Article> }) => (
-  <ul className="p-0 m-0 flex flex-col list-none gap-4">
+  <div className="p-0 m-0 flex list-none gap-4 flex-wrap">
     {posts.map((post) => (
-      <li key={post.slug} className="flex flex-col md:flex-row">
-        <span className="pr-4">
-          <FormattedDate date={post.pubDate} />
-        </span>
-        <Link href={`/posts/${post.slug}/`}>{post.title}</Link>
-      </li>
+      <Card className={cn("w-full md:w-[320px]")} key={post.slug}>
+        <Link href={`/posts/${post.slug}/`}>
+          <CardHeader>
+            <CardTitle className={cn("text-xl mt-0")}>{post.title}</CardTitle>
+            <CardDescription>
+              <p>{post.description}</p>
+            </CardDescription>
+          </CardHeader>
+          <CardFooter className={cn("mt-auto")}>
+            <small>
+              <FormattedDate date={post.pubDate} />
+            </small>
+          </CardFooter>
+        </Link>
+      </Card>
     ))}
-  </ul>
+  </div>
 );
